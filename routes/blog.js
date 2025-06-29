09c891f107597dfe2f38f68bfcdcb98e79df3a0a
ratna120 <ratnakumari20032006@gmail.com>
2025-06-29 11:00:40 +0530

    Fixed blog upload and form handling with multer

--- routes/blog.js
+++ routes/blog.js
@@ -1,61 +1,75 @@
-const { Router } = require("express");
+const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const { checkAuth } = require("../middlewares/authentication");
 
 const Blog = require("../models/blog");
 const Comment = require("../models/comment");
 
-const router = Router();
+const router = express.Router();
 
+// Set up multer to save uploaded files in /public/uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(`./public/uploads/`));
+    cb(null, "./public/uploads");
   },
   filename: function (req, file, cb) {
-    const fileName = `${Date.now()}-${file.originalname}`;
-    cb(null, fileName);
-  },
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage });
+
+// GET: Add Blog Page
+router.get("/add", checkAuth, (req, res) => {
+  res.render("add-blog");
 });
 
-const upload = multer({ storage: storage });
+// POST: Submit Blog
+router.post("/", checkAuth, upload.single("coverImage"), async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const coverImageURL = req.file ? `/uploads/${req.file.filename}` : null;
 
-router.get("/add-new", (req, res) => {
-  return res.render("addBlog", {
-    user: req.user,
-  });
+    await Blog.create({
+      title,
+      body,
+      coverImageURL,
+      createdBy: req.user._id,
+    });
+
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error creating blog:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
+// GET: View Single Blog
 router.get("/:id", async (req, res) => {
-  const blog = await Blog.findById(req.params.id).populate("createdBy");
-  const comments = await Comment.find({ blogId: req.params.id }).populate(
-    "createdBy"
-  );
-
-  return res.render("blog", {
-    user: req.user,
-    blog,
-    comments,
-  });
+  try {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({ blogId: blog._id }).populate("createdBy");
+    res.render("blog", { blog, comments });
+  } catch (err) {
+    console.error("Error fetching blog:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-router.post("/comment/:blogId", async (req, res) => {
-  await Comment.create({
-    content: req.body.content,
-    blogId: req.params.blogId,
-    createdBy: req.user._id,
-  });
-  return res.redirect(`/blog/${req.params.blogId}`);
-});
+// POST: Add Comment
+router.post("/comment/:id", checkAuth, async (req, res) => {
+  try {
+    const comment = await Comment.create({
+      content: req.body.content,
+      blogId: req.params.id,
+      createdBy: req.user._id,
+    });
 
-router.post("/", upload.single("coverImage"), async (req, res) => {
-  const { title, body } = req.body;
-  const blog = await Blog.create({
-    body,
-    title,
-    createdBy: req.user._id,
-    coverImageURL: `/uploads/${req.file.filename}`,
-  });
-  return res.redirect(`/blog/${blog._id}`);
+    res.redirect(`/blog/${req.params.id}`);
+  } catch (err) {
+    console.error("Error posting comment:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
